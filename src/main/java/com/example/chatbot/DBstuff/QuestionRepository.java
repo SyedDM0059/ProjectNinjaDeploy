5a1bc693c50697f3cf4e0a58824356c0c1fc6392
SyedDM0059 <105185076+SyedDM0059@users.noreply.github.com>
2022-07-06 12:43:17 +0800

    Delete QuestionRepository.java

--- src/main/java/com/example/chatbot/DBstuff/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chatbot.DBstuff;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuestionRepository extends MongoRepository<Question, String>, CustomQuestionRepository { // second must follow what id is, int or string
-
-}
